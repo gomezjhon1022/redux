4e3b1fb497e0bafbf376667416d99c6fea2ee66e
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-11-07 10:09:51 -0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Col } from 'antd';
 import { Searcher } from './components/Searcher'
+import { PokemonList } from './components/PokemonList';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
+      <PokemonList />
     </div>
   );
 }
