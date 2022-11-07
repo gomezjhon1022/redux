fa1dd275264275b8df4d77fd25585ef8279c9338
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-11-07 16:52:02 -0500

    Add middlewares and redux Devtools

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { pokemonsReducer } from './reducers/pokemons';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import { featuring, logger } from './middlewares/index';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(pokemonsReducer);
+const composedEnhancers = compose( window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(logger, featuring ));
+const store = createStore(
+  pokemonsReducer, composedEnhancers);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
