46ddf314cf65e777f67babf84b1cc052fb1b1dca
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-11-07 12:54:48 -0500

    Add redux integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Col } from 'antd';
 import { Searcher } from './components/Searcher'
 import { PokemonList } from './components/PokemonList';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { getPokemon } from './api';
 import logo from './statics/logo.svg';
+import { connect } from 'react-redux'
+import { setPokemons as setPokemonsActions } from './actions';
 import './App.css';
 
-function App() {
-  const [ pokemons, setPokemons ] = useState([]);
+function App({ pokemons, setPokemons}) {
 
+console.log(pokemons)
   useEffect(() => {
       const fetchPokemons = async () => {
         const pokemonsRes = await getPokemon();
@@ -30,4 +32,13 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
