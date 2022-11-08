fba9b5961999edf854f78fba92cf233e1df09b4d
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-11-07 21:25:12 -0500

    Add favorite button

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,21 @@
-import { StarOutlined } from '@ant-design/icons';
+import { StarButton } from './StarButton'
 import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
+import { useDispatch } from 'react-redux';
+import { setFavorite } from '../actions'
 
-const PokemonCard = ({name, image}) => {
+const PokemonCard = ({name, image, types, id, favorite }) => {
+  const dispatch = useDispatch();
+  const typesString = types.map(Element => Element.type.name).join (', ')
+  const handleOnFavorite = () => {
+    dispatch(setFavorite({pokemonId: id}))
+  }
   return (<Card
     title={name}
     cover={<img src={image} alt={name} />}
-    extra={<StarOutlined />}
+    extra={<StarButton isFavorite={favorite} onClick={handleOnFavorite}/>}
   >
-    <Meta description='fire, magic'/>
+    <Meta description={typesString}/>
   </Card>
   );
 };
